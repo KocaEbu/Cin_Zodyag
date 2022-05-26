b22e9d5fa86c5bbb5d15c036e282b313c404cc48
ekoca <ebubekirkoca@hotmail.com>
2022-05-26 16:54:25 +0300

    "Çin zodyağı hazır..."

---
+++ cin_zodyagi.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class cin_zodyagi {
+    public static void main(String[] args) {
+        int yil;
+        int zod;
+
+        Scanner input= new Scanner(System.in);
+
+        System.out.println("Dogum yilinizi giriniz : ");
+        yil= input.nextInt();
+        zod=yil%12;
+
+        switch(zod){
+
+            case 0 :
+                System.out.println("Maymun");
+                break;
+
+            case 1 :
+                System.out.println("Horoz");
+                break;
+
+            case 2:
+                System.out.println("Kopek");
+                break;
+
+            case 3:
+                System.out.println("Domuz");
+                break;
+
+            case 4:
+                System.out.println("Fare");
+                break;
+
+            case 5:
+                System.out.println("Okuz");
+                break;
+
+            case 6:
+                System.out.println("Kaplan");
+                break;
+
+            case 7:
+                System.out.println("Tavsan");
+                break;
+
+            case 8:
+                System.out.println("Ejderha");
+                break;
+
+            case 9:
+                System.out.println("Yilan");
+                break;
+
+            case 10:
+                System.out.println("At");
+                break;
+
+            case 11:
+                System.out.println("Koyun");
+                break;
+
+            default:
+                System.out.println("Dogum yilinizi dogru giriniz...");
+
+        }
+
+    }
+
+}
